27588fbc4c38cce1f5e97867a2a70fbe39f9ca87
Wishhhhh <86937408+Wu-yu-shiun@users.noreply.github.com>
2023-06-25 11:56:36 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,15 +2,20 @@ from flask import (Flask, request, abort)
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
+from config import channel_access_token,channel_secret,my_user_id
 import os
-import configparser
+
+# import configparser
+# config = configparser.ConfigParser()
+# config.read("config.ini")
+# line_bot_api = LineBotApi(config['line_bot']['channel_access_token'])
+# handler = WebhookHandler(config['line_bot']['channel_secret'])
+# line_bot_api.push_message(config['line_bot']['my_user_id'], TextSendMessage(text='你可以開始了'))
 
 app = Flask(__name__)
-config = configparser.ConfigParser()
-config.read("config.ini")
-line_bot_api = LineBotApi(config['line_bot']['channel_access_token'])
-handler = WebhookHandler(config['line_bot']['channel_secret'])
-line_bot_api.push_message(config['line_bot']['my_user_id'], TextSendMessage(text='你可以開始了'))
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+line_bot_api.push_message(my_user_id, TextSendMessage(text='你可以開始了'))
 # line_bot_api = LineBotApi('K/NEmvRBi3EfdNVOWUGxmBLXSkE9iIYjz2SXUHV7ioYLfD6FuZe+Y8J7GwSQSDW2kK04+p73qdK97Q1PhCScwN/KdHvSmQRrnIzuZCeIYeSqkhOUvSIQd5uIhetQkrKEbQAh6ZaZL5txW62L6axetAdB04t89/1O/w1cDnyilFU=')
 # handler = WebhookHandler('cb25265c3668e1983601ae8c62705ea3')
 # line_bot_api.push_message('Udb21c2cdfce57278db519a0b88153d82', TextSendMessage(text='你可以開始了'))
@@ -34,7 +39,7 @@ def callback():
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token,message)
-    
+
 #主程式
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
